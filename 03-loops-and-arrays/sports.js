0583b6b9664f9320b154df3d7859eb03c1e03322
alexhenao.001 <alexhenao.001@gmail.com>
2024-01-07 23:14:11 -0500

    push onto array

--- 03-loops-and-arrays/sports.js
+++ 03-loops-and-arrays/sports.js
@@ -1,10 +1,13 @@
 var sportsOne = ["Futbol", "Cricket", "Tennis", "Football"];
-for (var i = 0; i < sportsOne.length; i++) {
-    console.log(sportsOne[i]);
-}
+var sportsTwo = ["Cricket", "Tennis", "Football"];
+sportsTwo.push("Baseball");
+sportsTwo.push("Basketball");
+// for (let i=0; i < sportsOne.length; i++) {
+//     console.log(sportsOne[i]);
+// }
 // simplify the for loop
-for (var _i = 0, sportsOne_1 = sportsOne; _i < sportsOne_1.length; _i++) {
-    var tempSport = sportsOne_1[_i];
+for (var _i = 0, sportsTwo_1 = sportsTwo; _i < sportsTwo_1.length; _i++) {
+    var tempSport = sportsTwo_1[_i];
     if (tempSport == "Futbol") {
         console.log(tempSport + " << My Favorite!");
     }
--- 03-loops-and-arrays/sports.ts
+++ 03-loops-and-arrays/sports.ts
@@ -1,11 +1,15 @@
 let sportsOne: string[] = ["Futbol", "Cricket", "Tennis", "Football"];
+let sportsTwo: string[] = [ "Cricket", "Tennis", "Football"];
 
-for (let i=0; i < sportsOne.length; i++) {
-    console.log(sportsOne[i]);
-}
+sportsTwo.push("Baseball");
+sportsTwo.push("Basketball");
+
+// for (let i=0; i < sportsOne.length; i++) {
+//     console.log(sportsOne[i]);
+// }
 
 // simplify the for loop
-for (let tempSport of sportsOne) {
+for (let tempSport of sportsTwo) {
     if (tempSport == "Futbol") {
         console.log(tempSport + " << My Favorite!");
     } else {
