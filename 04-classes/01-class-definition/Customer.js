274a447ecff4c015759198d01697dcf72757463a
alexhenao.001 <alexhenao.001@gmail.com>
2024-01-07 23:39:12 -0500

    Add getter/setters

--- 04-classes/01-class-definition/Customer.js
+++ 04-classes/01-class-definition/Customer.js
@@ -1,8 +1,28 @@
 var Customer = /** @class */ (function () {
-    function Customer(theFirst, theLast) {
-        this.firstName = theFirst;
-        this.lastName = theLast;
+    function Customer(_firstName, _lastName) {
+        this._firstName = _firstName;
+        this._lastName = _lastName;
     }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Customer;
 }());
 //create an instance
--- 04-classes/01-class-definition/Customer.ts
+++ 04-classes/01-class-definition/Customer.ts
@@ -1,12 +1,30 @@
 class Customer {
 
-    firstName: string;
-    lastName: string;
+    private _firstName: string;
+    private _lastName: string;
 
-    constructor(theFirst: string, theLast: string) {
-        this.firstName = theFirst;
-        this.lastName = theLast;
+    constructor(_firstName: string, _lastName: string) {
+        this._firstName = _firstName;
+        this._lastName = _lastName;
     }
+
+    get firstName(): string {
+        return this._firstName;
+    }
+
+    get lastName(): string {
+        return this._lastName;
+    }
+
+   set firstName(value: string) {
+        this._firstName = value;
+    }
+
+    set lastName(value: string) {
+        this._lastName = value;
+    }
+
+
 }
 
 //create an instance
