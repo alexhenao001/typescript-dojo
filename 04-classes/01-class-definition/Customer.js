f90b74905e133595b2c70b76073d89bc3aeb9398
alexhenao.001 <alexhenao.001@gmail.com>
2024-01-07 23:29:15 -0500

    add constructor

--- 04-classes/01-class-definition/Customer.js
+++ 04-classes/01-class-definition/Customer.js
@@ -1,11 +1,11 @@
 var Customer = /** @class */ (function () {
-    function Customer() {
+    function Customer(theFirst, theLast) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
     }
     return Customer;
 }());
 //create an instance
-var myCustomer = new Customer();
-myCustomer.firstName = "Alex";
-myCustomer.lastName = "Henao";
+var myCustomer = new Customer("Alex", "Henao");
 console.log("myCustomer.firstName: " + myCustomer.firstName);
 console.log("myCustomer.lastName: " + myCustomer.lastName);
--- 04-classes/01-class-definition/Customer.ts
+++ 04-classes/01-class-definition/Customer.ts
@@ -2,14 +2,16 @@ class Customer {
 
     firstName: string;
     lastName: string;
+
+    constructor(theFirst: string, theLast: string) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
+    }
 }
 
 //create an instance
 
-let myCustomer = new Customer();
-
-myCustomer.firstName = "Alex";
-myCustomer.lastName = "Henao";
+let myCustomer = new Customer("Alex", "Henao");
 
 console.log("myCustomer.firstName: " + myCustomer.firstName);
 console.log("myCustomer.lastName: " + myCustomer.lastName);
