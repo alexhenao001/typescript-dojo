f953a61b45c262e1179c184b707f68f81539c02c
alexhenao.001 <alexhenao.001@gmail.com>
2024-01-07 22:54:50 -0500

    add sample-types.ts and compile

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Alex";
+var lastName = "Henao";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// use template Strings
+console.log("Hi ".concat(firstName, " ").concat(lastName));
