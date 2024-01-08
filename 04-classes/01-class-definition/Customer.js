778bcbc877dd19331494ebec6860daa33b8c3cd9
alexhenao.001 <alexhenao.001@gmail.com>
2024-01-08 00:07:08 -0500

    remove access modifiers from 01-class-definition

--- 04-classes/01-class-definition/Customer.js
+++ 04-classes/01-class-definition/Customer.js
@@ -1,31 +1,13 @@
 var Customer = /** @class */ (function () {
-    function Customer(_firstName, _lastName) {
-        this._firstName = _firstName;
-        this._lastName = _lastName;
+    function Customer(theFirst, theLast) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
     }
-    Object.defineProperty(Customer.prototype, "firstName", {
-        get: function () {
-            return this._firstName;
-        },
-        set: function (value) {
-            this._firstName = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Customer.prototype, "lastName", {
-        get: function () {
-            return this._lastName;
-        },
-        set: function (value) {
-            this._lastName = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
     return Customer;
 }());
 //create an instance
 var myCustomer = new Customer("Alex", "Henao");
+myCustomer.firstName = "Alex";
+myCustomer.lastName = "Henao";
 console.log("myCustomer.firstName: " + myCustomer.firstName);
 console.log("myCustomer.lastName: " + myCustomer.lastName);
--- 04-classes/01-class-definition/Customer.ts
+++ 04-classes/01-class-definition/Customer.ts
@@ -1,35 +1,20 @@
 class Customer {
 
-    private _firstName: string;
-    private _lastName: string;
+    firstName: string;
+    lastName: string;
 
-    constructor(_firstName: string, _lastName: string) {
-        this._firstName = _firstName;
-        this._lastName = _lastName;
+    constructor(theFirst: string, theLast: string) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
     }
-
-    get firstName(): string {
-        return this._firstName;
-    }
-
-    get lastName(): string {
-        return this._lastName;
-    }
-
-   set firstName(value: string) {
-        this._firstName = value;
-    }
-
-    set lastName(value: string) {
-        this._lastName = value;
-    }
-
-
 }
 
 //create an instance
 
 let myCustomer = new Customer("Alex", "Henao");
+
+myCustomer.firstName = "Alex";
+myCustomer.lastName = "Henao";
 
 console.log("myCustomer.firstName: " + myCustomer.firstName);
 console.log("myCustomer.lastName: " + myCustomer.lastName);
